57a2b45da54e90b4e8309744eacc20bede42b6b4
Tifan Dwi Avianto <ifan.anvity@gmail.com>
2019-02-09 14:25:44 +0700

    Add files via upload

---
+++ ems.c
@@ -0,0 +1,20 @@
+/*
+==============================================================================
+Version : 1.0 (25/5/2018)
+          - intial release
+          1.2 (2/6/2018)
+          - ditambahkan preview daftar kegiatan pada menu Hapus Kegiatan, 
+            supaya penguna dapat lebih mudah memilih no. urut
+          - fixed peletakan koordinat output yang tidak sesuai
+Desc    : Tugas akhir
+==============================================================================
+*/
+
+#include "header.h"
+
+int main(void) {
+    printTitle();
+    mainMenu();
+    
+    return EXIT_SUCCESS;
+}
